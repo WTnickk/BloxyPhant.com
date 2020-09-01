8294335a1b9eb9e8212830e99325bb7533838aa2
WTnickk <69070826+WTnickk@users.noreply.github.com>
2020-09-01 16:37:45 -0700

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,6 +1,8 @@
 <html>  
 <body>
 
+  <?php  ?><h2>Add a user</h2>
+
   <form method="post">
     	<label for="username">Username</label>
     	<input type="text" name="username" id="username">
@@ -8,6 +10,7 @@
     	<input type="passowrd" name="username" id="username">
     	<input type="submit" name="submit" value="Submit">
     </form>
+    <?php  ?>
 
 </body>
 </html>
